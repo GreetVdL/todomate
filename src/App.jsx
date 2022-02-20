d21407f462b3b4d117e9f542b44742d52aba95bd
Greet Van der Linden <vanderlindengreet@gmail.com>
2022-02-20 08:28:48 +0100

    add header animation

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import logo from "./logo.svg";
 import "./App.scss";
 import Form from "./components/Form";
 import List from "./components/List";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 function App() {
+  const headerRef = useRef();
+
+  useEffect(() => {
+    headerRef.current.style.animation =
+      "slide-in-top 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both";
+    return () => {
+      headerRef.current.style.animation = "none";
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header>TO DO MATE</header>
+      <header ref={headerRef}>TO DO MATE</header>
       <Form></Form>
       <List />
     </div>
